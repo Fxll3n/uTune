a8cac605c7dabdb6b810d0350bcbdf03f8e849d4
Angel Bitsov <angel.bitsov@gmail.com>
2025-10-23 10:09:25 -0500

    Initial Commit

---
+++ uTune/uTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uTuneApp.swift
+//  uTune
+//
+//  Created by Angel Bitsov on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct uTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
